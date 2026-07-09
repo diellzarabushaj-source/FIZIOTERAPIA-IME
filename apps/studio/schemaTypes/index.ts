5a1121797b876a0a4f60035eb1e3b108aa56fca1
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-09 13:08:15 +0200

    Register FAQ and legal Sanity schemas

--- apps/studio/schemaTypes/index.ts
+++ apps/studio/schemaTypes/index.ts
@@ -1,6 +1,8 @@
 import { authorType } from "./author";
 import { blockContentType } from "./blockContent";
 import { categoryType } from "./category";
+import { faqItemType } from "./faqItem";
+import { legalPageType } from "./legalPage";
 import { postType } from "./post";
 
-export const schemaTypes = [postType, authorType, categoryType, blockContentType];
+export const schemaTypes = [postType, faqItemType, legalPageType, authorType, categoryType, blockContentType];
