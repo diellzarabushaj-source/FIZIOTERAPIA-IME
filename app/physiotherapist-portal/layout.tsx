341f76384817b639e066b983a8ad15b3c6e4c0dc
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-10 18:33:49 +0200

    Use professional multi-page dashboard shell

--- app/physiotherapist-portal/layout.tsx
+++ app/physiotherapist-portal/layout.tsx
@@ -1,6 +1,7 @@
 import { requirePhysioActor } from "@/lib/backend/access";
+import { DashboardShell } from "./DashboardShell";
 
 export default async function PhysiotherapistPortalLayout({ children }: { children: React.ReactNode }) {
   await requirePhysioActor();
-  return <>{children}</>;
+  return <DashboardShell>{children}</DashboardShell>;
 }
