20e3ad6ac0a22cc3f7afbb757be6c27774832e22
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-10 15:57:17 +0200

    Load FAQ explorer styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ import "./public-site-fixes.css";
 import "./patient-simple.css";
 import "./public-design-system.css";
 import "./support-center.css";
+import "./faq-explorer.css";
 
 const siteUrl = process.env.NEXT_PUBLIC_APP_URL || "https://fizioterapia-ime.vercel.app";
 
