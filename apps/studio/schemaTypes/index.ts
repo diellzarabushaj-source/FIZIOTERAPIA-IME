874e443448a088f754e89a304a185e6b0cd7c945
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-11 05:33:02 +0200

    feat: register production content types in local Studio

--- apps/studio/schemaTypes/index.ts
+++ apps/studio/schemaTypes/index.ts
@@ -1,8 +1,25 @@
+import { appScreenType } from "./appScreen";
 import { authorType } from "./author";
 import { blockContentType } from "./blockContent";
 import { categoryType } from "./category";
 import { faqItemType } from "./faqItem";
+import { featureCardType } from "./featureCard";
 import { legalPageType } from "./legalPage";
+import { pageSectionType } from "./pageSection";
 import { postType } from "./post";
+import { pricingPlanType } from "./pricingPlan";
+import { sitePageType } from "./sitePage";
 
-export const schemaTypes = [postType, faqItemType, legalPageType, authorType, categoryType, blockContentType];
+export const schemaTypes = [
+  postType,
+  faqItemType,
+  legalPageType,
+  sitePageType,
+  featureCardType,
+  pricingPlanType,
+  appScreenType,
+  authorType,
+  categoryType,
+  pageSectionType,
+  blockContentType,
+];
