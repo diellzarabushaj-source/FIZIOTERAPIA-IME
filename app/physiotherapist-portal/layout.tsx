103e12331070730e943bc7cd26f22d956aa4b60a
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-11 04:33:11 +0200

    Load global dashboard search styles

--- app/physiotherapist-portal/layout.tsx
+++ app/physiotherapist-portal/layout.tsx
@@ -2,6 +2,7 @@ import { DashboardShell } from "./DashboardShell";
 import "./dashboard.css";
 import "./dashboard-polish.css";
 import "./dashboard-mobile.css";
+import "./dashboard-search.css";
 
 export default function PhysiotherapistPortalLayout({ children }: { children: React.ReactNode }) {
   return <DashboardShell>{children}</DashboardShell>;
