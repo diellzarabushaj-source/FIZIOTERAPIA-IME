616ecaad7c2c5e6e31dbb755d2ee67b927c26821
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-11 04:31:14 +0200

    Load mobile physiotherapist navigation styles

--- app/physiotherapist-portal/layout.tsx
+++ app/physiotherapist-portal/layout.tsx
@@ -1,6 +1,7 @@
 import { DashboardShell } from "./DashboardShell";
 import "./dashboard.css";
 import "./dashboard-polish.css";
+import "./dashboard-mobile.css";
 
 export default function PhysiotherapistPortalLayout({ children }: { children: React.ReactNode }) {
   return <DashboardShell>{children}</DashboardShell>;
