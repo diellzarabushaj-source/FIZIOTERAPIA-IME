4e3dd6f47cbd15a845f8bc1dc26d1bf934fd85c4
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-11 04:35:54 +0200

    Load physiotherapist quick action menu styles

--- app/physiotherapist-portal/layout.tsx
+++ app/physiotherapist-portal/layout.tsx
@@ -3,6 +3,7 @@ import "./dashboard.css";
 import "./dashboard-polish.css";
 import "./dashboard-mobile.css";
 import "./dashboard-search.css";
+import "./dashboard-actions.css";
 
 export default function PhysiotherapistPortalLayout({ children }: { children: React.ReactNode }) {
   return <DashboardShell>{children}</DashboardShell>;
