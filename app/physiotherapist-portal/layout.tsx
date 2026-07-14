9774ad704fe6fcd32ff088eb2ad24fd789760b7a
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-14 11:35:12 +0200

    Load dashboard navigation layout polish

--- app/physiotherapist-portal/layout.tsx
+++ app/physiotherapist-portal/layout.tsx
@@ -5,6 +5,7 @@ import "./dashboard-mobile.css";
 import "./dashboard-search.css";
 import "./dashboard-actions.css";
 import "./dashboard-consistency.css";
+import "./dashboard-navigation.css";
 
 export default function PhysiotherapistPortalLayout({ children }: { children: React.ReactNode }) {
   return <DashboardShell>{children}</DashboardShell>;
